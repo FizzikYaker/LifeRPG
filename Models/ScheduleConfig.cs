6c87d3226d5da6e4ad9788f875492e3649853018
yaker2018 <yaker2018@yandex.ru>
2026-09-25 04:15:58 +1000

    Правка ошибок добавление графика

--- Models/ScheduleConfig.cs
+++ Models/ScheduleConfig.cs
@@ -10,13 +10,13 @@ public enum SchedulePatternType
 public class ScheduleConfig
 {
     public SchedulePatternType PatternType { get; set; } = SchedulePatternType.Shift2_2_2_2;
-    public DateTime AnchorDate { get; set; } = DateTime.Today;
-
-    // Используем TimeOnly? для точного совпадения с HTML input[type="time"]
-    public TimeOnly? DayShiftWakeTime { get; set; } = new TimeOnly(6, 20);
-    public TimeOnly? NightShiftWakeTime { get; set; } = new TimeOnly(18, 10);
-
+    public DateTime AnchorDate { get; set; } = new DateTime(2026, 9, 1);
+    public TimeSpan DayShiftWakeTime { get; set; } = new TimeSpan(6, 20, 0);
+    public TimeSpan NightShiftWakeTime { get; set; } = new TimeSpan(18, 10, 0);
     public int ShiftDurationHours { get; set; } = 12;
     public int CommuteHours { get; set; } = 2;
     public int MaxWorkloadPercent { get; set; } = 80;
+
+    public int WorkDaysInCycle { get; set; } = 1;  // например "1" в 1/4
+    public int OffDaysInCycle { get; set; } = 4;    // например "4" в 1/4
 }
